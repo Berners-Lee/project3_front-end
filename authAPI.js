a6e2bb3ac92122f7c6c9b7c49e98bd87b9e63e5a
gotwilk890 <dave.wilk890@gmail.com>
2015-12-01 11:58:01 -0500

    Adds generic ajax method to be called by our requests.
    DW

--- authAPI.js
+++ authAPI.js
@@ -2,7 +2,15 @@
 
 var authAPI = {
 
-  api_url: 'https://localhost:3000'
+  api_url: 'https://localhost:3000',
+
+  ajax: function(config, cb){
+    $.ajax(config).done(function(data, textStatus, jqhxr){
+      cb(null, data);
+    }).fail(function(jqhxr, status, error){
+      cb({jqhxr: jqhxr, statur: status, error: error});
+      });
+    },
 
 };
 
