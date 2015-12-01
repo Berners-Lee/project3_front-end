b34a3525d350d12f0891259a44848ad9938e595f
gotwilk890 <dave.wilk890@gmail.com>
2015-12-01 18:08:37 -0500

    Adds handlebar fuctionality
    Adds handlebars to access our database and grab product info.

---
+++ js/handlebars-template.js
@@ -0,0 +1,15 @@
+$('#products-show').click(function(e){
+  $.ajax({
+    method: "GET",
+    url: "http://localhost:3000/products",
+    dataType: "json"
+  }).done(function(data){
+    console.log(JSON.stringify(data,null,4));
+    var productIndexTemplate = Handlebars.compile($('#product-index').html());
+    var productHTML = productIndexTemplate({product:data});
+    $('#populate-products').append(productHTML);
+  }).fail(function(data){
+    console.error(data);
+  });
+
+});
