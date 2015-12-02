8cf91cedd8f8156a0cdb38e3eb4fa38a5c849ff9
gotwilk890 <dave.wilk890@gmail.com>
2015-12-02 11:13:53 -0500

    Starts server call for create order upon purchase
    DW

---
+++ js/checkout.js
@@ -0,0 +1,46 @@
+'use strict'
+
+var histAPI = {
+
+  api_url: 'http://localhost:3000',
+
+  ajax: function(config, cb){
+    $.ajaxSetup({
+      xhrFields: {
+        withCredentials: true
+      }
+    });
+    $.ajax(config).done(function(data, textStatus, jqhxr){
+      cb(null, data);
+    }).fail(function(jqhxr, status, error){
+      cb({jqhxr: jqhxr, statur: status, error: error});
+    });
+  },
+
+  createOrder: function(callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.api_url + '/orders',
+      dataType: 'json'
+    }, callback);
+  }
+
+}
+
+$(document).ready(function(){
+
+$('#cart-show').on('click', function(e) {
+    var cb = function cb(error, data) {
+      if (error) {
+        callback(error);
+        return;
+      }
+      callback(null, data);
+    };
+    histAPI.showShopHistory(cb);
+    e.preventDefault();
+  });
+
+
+
+});
