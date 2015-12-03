930db978c7bce980e9ad26960eebd22e6fb5a9a6
gotwilk890 <dave.wilk890@gmail.com>
2015-12-03 14:12:05 -0500

    Adds sort options for products.
    Users can now sort all the products by category.
    
    DW

--- js/handlebars-template.js
+++ js/handlebars-template.js
@@ -1,10 +1,70 @@
-$('#products-show').click(function(e){
+
+$('#all').click(function(e){
   $.ajax({
     method: "GET",
     url: "http://localhost:3000/products",
     dataType: "json"
   }).done(function(data){
-    console.log(JSON.stringify(data,null,4));
+    var productIndexTemplate = Handlebars.compile($('#product-index').html());
+    var productHTML = productIndexTemplate({product:data});
+    $('#populate-products').html('');
+    $('#populate-products').append(productHTML);
+  }).fail(function(data){
+    console.error(data);
+  });
+});
+
+$('#dorm').click(function(e){
+  $.ajax({
+    method: "GET",
+    url: "http://localhost:3000/products/dorm",
+    dataType: "json"
+  }).done(function(data){
+    var productIndexTemplate = Handlebars.compile($('#product-index').html());
+    var productHTML = productIndexTemplate({product:data});
+    $('#populate-products').html('');
+    $('#populate-products').append(productHTML);
+  }).fail(function(data){
+    console.error(data);
+  });
+});
+
+$('#school').click(function(e){
+  $.ajax({
+    method: "GET",
+    url: "http://localhost:3000/products/school",
+    dataType: "json"
+  }).done(function(data){
+    var productIndexTemplate = Handlebars.compile($('#product-index').html());
+    var productHTML = productIndexTemplate({product:data});
+    $('#populate-products').html('');
+    $('#populate-products').append(productHTML);
+  }).fail(function(data){
+    console.error(data);
+  });
+});
+
+$('#fun').click(function(e){
+  $.ajax({
+    method: "GET",
+    url: "http://localhost:3000/products/fun",
+    dataType: "json"
+  }).done(function(data){
+    var productIndexTemplate = Handlebars.compile($('#product-index').html());
+    var productHTML = productIndexTemplate({product:data});
+    $('#populate-products').html('');
+    $('#populate-products').append(productHTML);
+  }).fail(function(data){
+    console.error(data);
+  });
+});
+
+$('#food').click(function(e){
+  $.ajax({
+    method: "GET",
+    url: "http://localhost:3000/products/food",
+    dataType: "json"
+  }).done(function(data){
     var productIndexTemplate = Handlebars.compile($('#product-index').html());
     var productHTML = productIndexTemplate({product:data});
     $('#populate-products').html('');
